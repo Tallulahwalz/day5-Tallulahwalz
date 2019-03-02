42697b709dd85496a338490b3b2a137b10f89d13
Tallulahwalz <tmwalznyc@gmail.com>
2019-03-02 17:22:55 +0000

    YAY!!

--- problemSetDay5.py
+++ problemSetDay5.py
@@ -1,1 +1,8 @@
 # File name: problemSetDay5.py
+print ('Your player Strength is 5!')
+strength = 5
+while strength <= 10:
+    print (strength)
+    strength +=1
+
+print ("you have grown too strong and have moved on to the next level of the game!")
